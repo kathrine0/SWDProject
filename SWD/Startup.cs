b3c3210a6a7ebf704fbcf653d53f581be09dcbe8
Katarzyna Biernat <k.biernat@lgbs.pl>
2015-05-17 18:20:53 +0200

    init

---
+++ SWD/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SWD.Startup))]
+namespace SWD
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
