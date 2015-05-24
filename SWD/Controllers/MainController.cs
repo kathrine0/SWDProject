8b0f45257e71079c2e76d6691b4f5c34679879ad
Katarzyna Biernat <k.biernat@lgbs.pl>
2015-05-24 14:52:58 +0200

    layout layout

--- SWD/Controllers/MainController.cs
+++ SWD/Controllers/MainController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace SWD.Controllers
 {
-    public class HomeController : Controller
+    public class MainController : Controller
     {
         public ActionResult Index()
         {
