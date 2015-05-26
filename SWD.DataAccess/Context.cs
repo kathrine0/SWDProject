a69fb475ee58f0f9faa33100cb54df292d2efc92
Katarzyna Biernat <k.biernat@lgbs.pl>
2015-05-26 18:44:44 +0200

    Jest baza, jest impreza

---
+++ SWD.DataAccess/Context.cs
@@ -0,0 +1,20 @@
+﻿using SWD.DataAccess.Entites;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWD.DataAccess
+{
+    class Context : DbContext
+    {
+        public Context()
+            : base("DefaultConnection")
+        {
+
+        }
+        public DbSet<Destination> Blogs { get; set; }
+    }
+}
---
+++ SWD.DataAccess/Entites/Destination.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWD.DataAccess.Entites
+{
+    public class Destination
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
