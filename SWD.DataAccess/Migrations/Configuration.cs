1311e7bbaeeecab6912738c3dbfa115ba20b68f8
Katarzyna Biernat <k.biernat@lgbs.pl>
2015-06-02 20:07:27 +0200

    luźne dywagacje na projekcie

--- SWD.DataAccess/Migrations/Configuration.cs
+++ SWD.DataAccess/Migrations/Configuration.cs
@@ -21,7 +21,21 @@
                 new FormulaElementary(2, "kierowca otrzymuje mandat", false, FormulaElementaryType.Exit),
                 new FormulaElementary(3, "Prędkość >180 km/h", false, FormulaElementaryType.Enter),
                 new FormulaElementary(4, "Występuje niebezpieczeństwo", false, FormulaElementaryType.Other),
-                new FormulaElementary(5, "moc silnika jest poniżej 70 KM", true, FormulaElementaryType.Enter)
+                new FormulaElementary(5, "moc silnika jest poniżej 70 KM", true, FormulaElementaryType.Enter),
+
+                new FormulaElementary(6, "Wiek > 20", true, FormulaElementaryType.Other),
+                new FormulaElementary(7, "Wiek > 30", true, FormulaElementaryType.Other),
+                new FormulaElementary(8, "Wiek > 40", true, FormulaElementaryType.Other),
+                new FormulaElementary(9, "Wiek > 50", true, FormulaElementaryType.Other),
+                new FormulaElementary(10, "Wiek > 60", true, FormulaElementaryType.Other),
+                new FormulaElementary(11, "Wiek > 70", true, FormulaElementaryType.Other),
+
+                new FormulaElementary(20, "Obywatelstwo = Polskie", true, FormulaElementaryType.Enter),
+                new FormulaElementary(21, "Obywatelstwo = Niemieckie", true, FormulaElementaryType.Enter),
+                new FormulaElementary(21, "Obywatelstwo = Brytyjskie", true, FormulaElementaryType.Enter),
+                new FormulaElementary(21, "Obywatelstwo = Amerykańskie", true, FormulaElementaryType.Enter),
+
+
             };
 
             context.FormulaElementaries.AddOrUpdate(elementaries.ToArray());
